b86ee8eca0c035e90359e5b5a00ac41b0d03cb6a
friends-xiaohuli <2120938739@qq.com>
2025-12-17 19:19:39 +0800

    push

--- server.bat
+++ server.bat
@@ -1,1 +1,5 @@
 node server.js
+
+@REM npm run dev
+@REM npm run build
+@REM npm run preview
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
-
+  
   // 关键：用于 Vercel + 前端路由
   build: {
     outDir: 'dist'
